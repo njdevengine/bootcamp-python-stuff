842f50726259b2429d14e85b4f7793183044b6d1
njdevengine <newjerseydevelopment@gmail.com>
2019-01-31 21:21:22 -0500

    opens csv via os

---
+++ findfile.py
@@ -0,0 +1,10 @@
+import os
+
+#import a package to read in csv
+
+import csv
+
+#create the path for the file
+#same as path = '../Resources/accounting.csv
+csvpath = os.path.join('..','Resources','accounting.csv')
+print(csvpath)
